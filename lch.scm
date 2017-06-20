d79bfb355447fe533bf3d8164fc75ebfbef4bafe
gregr <greg.weiqi@gmail.com>
2017-06-20 12:43:41 -0400

    Define lattice representations

--- lch.scm
+++ lch.scm
@@ -45,6 +45,55 @@
 ;; Worries:
 ;; Is this going to end up gravitating towards being a general SMT solver?
 
+(define-syntax defrecord
+  (syntax-rules ()
+    ((_ name name?)
+     (begin
+       (define name (vector 'name))
+       (define (name? datum) (eq? name datum))))
+    ((_ name name? (field set-field) ...)
+     (begin
+       (define (name field ...) (vector 'name field ...))
+       (define (name? datum)
+         (and (vector? datum) (eq? 'name (vector-ref datum 0))))
+       (let ()
+         (define (range-assoc start xs)
+           (let loop ((xs xs) (idx start))
+             (if (null? xs)
+               '()
+               (cons (cons (car xs) idx) (loop (cdr xs) (+ idx 1))))))
+         (define (define-field-getter name rassc)
+           (define idx (cdr (assoc name rassc)))
+           (eval `(define (,name datum) (vector-ref datum ,idx))))
+         (define (define-field-setter name rassc)
+           (define idx (cdr (assoc name rassc)))
+           (eval `(define (,name datum value)
+                    (let ((new (vector-copy datum)))
+                      (vector-set! new ,idx value)
+                      new))))
+         (let ((fns (range-assoc 1 '(field ...))))
+           (begin (define-field-getter 'field fns) ...))
+         (let ((set-fns (range-assoc 1 '(set-field ...))))
+           (begin (define-field-setter 'set-field set-fns) ...)))))
+    ((_ name name? field ...)
+     (begin
+       (define (name field ...) (vector 'name field ...))
+       (define (name? datum)
+         (and (vector? datum) (eq? 'name (vector-ref datum 0))))
+       (let ()
+         (define (range-assoc start xs)
+           (let loop ((xs xs) (idx start))
+             (if (null? xs)
+               '()
+               (cons (cons (car xs) idx) (loop (cdr xs) (+ idx 1))))))
+         (define (define-field-getter name rassc)
+           (define idx (cdr (assoc name rassc)))
+           (eval `(define (,name datum) (vector-ref datum ,idx))))
+         (let ((fns (range-assoc 1 '(field ...))))
+           (begin (define-field-getter 'field fns) ...)))))))
+
+(define finite-set-empty '())
+
 ;; A numeric-set stores sorted, open intervals of the form (lb . ub) where lb
 ;; is the lower bound and ub is the upper bound.  The bounds may be #f,
 ;; representing negative and positive infinity, depending on which side they
@@ -53,3 +102,26 @@
 ;; represents a union of the point or the closed interval [2 2] with the
 ;; half-closed interval (4 8] and the open interval (10 +infinity), i.e., all
 ;; numbers x such that either x = 2 OR 4 < x <= 8 OR 10 < x).
+(define numeric-set-empty '())
+(define numeric-set-full '((#f . #f)))
+
+(define top #t)
+(define (top? a) (eq? top a))
+(defrecord type-union type-union?
+           tu-pair tu-symbol tu-number tu-nil tu-false tu-true)
+(defrecord type-pair type-pair?
+           type-pair-car
+           type-pair-cdr
+           type-pair-complement-fd?
+           type-pair-fd
+           type-pair-absents)
+(defrecord type-symbol type-symbol?
+           type-symbol-complement-fd?
+           type-symbol-fd)
+(defrecord type-number type-number?
+           type-number-integer?
+           type-number-set
+           type-number-arithmetic)
+(defrecord singleton singleton? singleton-value)
+(define bottom #f)
+(define bottom? not)
