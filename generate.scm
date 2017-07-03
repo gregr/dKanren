42331dfb8e68090d0f00104ec074adf2f9958667
gregr <greg.weiqi@gmail.com>
2017-07-02 22:30:01 -0400

    Set up lazy example streaming

--- generate.scm
+++ generate.scm
@@ -63,3 +63,35 @@
   (map
     (lambda (i) `(,(if (null? (run 1 (q) (test i))) 0 1) ,i))
     inputs))
+
+
+;; Lazy example streaming
+
+(define examples-current '())
+
+(define (examples-start generate)
+  (set! examples-current (query (i) (generate i))))
+
+(define (examples-next k)
+  (define (test example)
+    (eval (cons (car example) (map (lambda (x) `(quote ,x)) (cdr example)))))
+  (and (not (null? examples-current))
+       (let ((next (stream-next examples-current)))
+         (and (pair? next)
+              (set! examples-current (cdr next))
+              (let ((input (car (reify-initial (car next)))))
+                (k `(,(if (null? (run 1 (q) (test input))) 0 1) ,input)))
+              #t))))
+
+
+;; Stream unification examples
+
+(examples-start example-==)
+
+;; Optionally set n to the number of desired examples.
+(let loop ((n #f))
+  (if (and n (= 0 n))
+    #f
+    (begin
+      (and (examples-next (lambda (x) (printf "~s\n" x)))
+           (loop (and n (- n 1)))))))
