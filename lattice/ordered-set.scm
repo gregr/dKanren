1bb97add06c4f967e3d9f5840e3f89863b5fd68a
gregr <greg.weiqi@gmail.com>
2017-07-17 16:31:04 -0400

    Factor out order.scm

--- lattice/ordered-set.scm
+++ lattice/ordered-set.scm
@@ -1,46 +1,4 @@
-(define (vector-compare a b lt eq gt)
-  (define len-a (vector-length a))
-  (define len-b (vector-length b))
-  (cond
-    ((< len-a len-b) lt)
-    ((> len-a len-b) gt)
-    (else (pair-compare (vector->list a) (vector->list b) lt eq gt))))
-(define (pair-compare a b lt eq gt)
-  ((any-compare (car a) (car b) (lambda () lt)
-                (lambda () (any-compare (cdr a) (cdr b) lt eq gt))
-                (lambda () gt))))
-;; NOTE: this is only sound for non-generated symbols.
-(define (symbol-compare a b lt eq gt)
-  (cond
-    ((eq? a b) eq)
-    ((string<? (symbol->string a) (symbol->string b)) lt)
-    (else gt)))
-(define (string-compare a b lt eq gt)
-  (cond ((string<? a b) lt) ((string>? a b) gt) (else eq)))
-(define (number-compare a b lt eq gt)
-  (cond ((< a b) lt) ((> a b) gt) (else eq)))
-(define (boolean-compare a b lt eq gt) (if a (if b eq gt) (if b lt eq)))
-
-;; TODO: auto-generate this.
-(define (any-compare a b lt eq gt)
-  (if (vector? a) (if (vector? b) (vector-compare a b lt eq gt) lt)
-   (if (vector? b) gt
-     (if (pair? a) (if (pair? b) (pair-compare a b lt eq gt) lt)
-       (if (pair? b) gt
-         (if (symbol? a) (if (symbol? b) (symbol-compare a b lt eq gt) lt)
-           (if (symbol? b) gt
-             (if (string? a) (if (string? b) (string-compare a b lt eq gt) lt)
-               (if (string? b) gt
-                 (if (number? a)
-                   (if (number? b) (number-compare a b lt eq gt) lt)
-                   (if (number? b) gt
-                     (if (boolean? a)
-                       (if (boolean? b) (boolean-compare a b lt eq gt) lt)
-                       (if (boolean? b) gt
-                         (if (and (null? a) (null? b)) eq
-                           (error 'any-compare
-                                  (format "unsupported comparison of ~s and ~s"
-                                          a b))))))))))))))))
+(load "order.scm")
 
 (define (ordered-set-join xs ys)
   (if (null? xs) ys
