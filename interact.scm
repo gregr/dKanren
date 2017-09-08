5e1b120ae36f6be516e09a95e96c5d983c0e2a58
gregr <greg.weiqi@gmail.com>
2017-09-08 11:02:05 -0400

    Test interaction with (q-np 5)

--- interact.scm
+++ interact.scm
@@ -18,30 +18,41 @@
 (define q-quine (query (p) (evalo p p)))
 
 
-(define (in)
-  (printf "enter a path: ")
-  (read))
+;(define (in)
+  ;(printf "enter a path: ")
+  ;(read))
 
-(define (show ss)
-  (pretty-print (or (cadr (stream-pretty ss)) ss))
-  (newline))
+;(define (show ss)
+  ;(pretty-print (or (cadr (stream-pretty ss)) ss))
+  ;(newline))
 
-(define (out flag) (printf "result: ~s\n" flag))
+;(define (out flag) (printf "result: ~s\n" flag))
 
-(define ss (simplify (query (x)
-                       (conde
-                         ((== 1 3))
-                         ((== 2 x) (== 3 x))
-                         ((conde
-                            ((== 3 x) (== 4 x))
-                            ((== 5 x)))
-                          (conde
-                            ((== 1 1))
-                            ((== #t #t)))
-                          (conde
-                            ((== #f #t))
-                            ((== #t #t))))))))
+;(define ss (simplify (query (x)
+                       ;(conde
+                         ;((== 1 3))
+                         ;((== 2 x) (== 3 x))
+                         ;((conde
+                            ;((== 3 x) (== 4 x))
+                            ;((== 5 x)))
+                          ;(conde
+                            ;((== 1 1))
+                            ;((== #t #t)))
+                          ;(conde
+                            ;((== #f #t))
+                            ;((== #t #t))))))))
 
-;(define ss (simplify (q-np 5)))
+(define (in) (map (lambda (n)
+                    (cond
+                      ((= 0 n) #t)
+                      ((= 1 n) #f)
+                      (else (error 'in (format "invalid path segment ~s" n)))))
+                  (read)))
+
+(define (show ss) (printf "~s\n" (cadr (stream-pretty ss))))
+
+(define (out flag) (printf "~s\n" flag))
+
+(define ss (simplify (q-np 5)))
 
 (interact in show out ss ss)
