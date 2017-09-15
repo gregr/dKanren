2e765404ac63866ddc515fd167899dc7cca220a5
gregr <greg.weiqi@gmail.com>
2017-09-15 13:11:28 -0400

    Support good-path requests

--- interact.scm
+++ interact.scm
@@ -42,19 +42,32 @@
                             ;((== #f #t))
                             ;((== #t #t))))))))
 
-(define (in) (map (lambda (n)
-                    (cond
-                      ((= 0 n) #t)
-                      ((= 1 n) #f)
-                      (else (error 'in (format "invalid path segment ~s" n)))))
-                  (read)))
+(define (in)
+  (define request (read))
+  (cond
+    ((eq? 'good-path request) 'good-path)
+    ((pair? request)
+     (map (lambda (n)
+            (cond
+              ((= 0 n) #t)
+              ((= 1 n) #f)
+              (else (error 'in (format "invalid path segment ~s" n)))))
+          request))
+    (else request)))
 
 (define (show ss) (printf "~s\n" (cadr (stream-pretty ss))))
 
-(define (out flag) (printf "~s\n" flag))
+(define (out response)
+  (define output
+    (cond
+      ((eq? 'good-path (car response))
+       (map (lambda (b) (if b 0 1)) (cadr response)))
+      ((eq? 'follow-path (car response)) (cadr response))
+      (else (error 'out (format "unrecognized output: ~s" response)))))
+  (printf "~s\n" output))
 
 (define (read-query) (simplify (eval (read))))
 
 (define ss (read-query))
 
-(interact in show out ss ss)
+(interact in show out ss ss #f #t)
